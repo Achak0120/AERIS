4063a694b887c8072c58a7b3c7b48c4e136664f1
Aishik C. <achak0120@gmail.com>
2025-06-04 15:43:28 -0500

    blue cube dataset

--- src/ai_model/model.py
+++ src/ai_model/model.py
@@ -1,10 +1,3 @@
 import torch
-import torch.nn as nn
-import torch.nn.functional as F
-from typing import Tuple
-import torchvision.transforms as T
-from torchvision import models
-import logging
 
 
- 
