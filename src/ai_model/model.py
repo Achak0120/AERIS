92a31338e74e17e5071d1be003418149cf745cf8
achak <136048425+Achak0120@users.noreply.github.com>
2025-06-04 14:56:53 -0500

    libraries

--- src/ai_model/model.py
+++ src/ai_model/model.py
@@ -2,8 +2,9 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from typing import Tuple
-import torchvision.transform as T
+import torchvision.transforms as T
 from torchvision import models
 import logging
 
 
+ 
