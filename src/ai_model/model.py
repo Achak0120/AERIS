e52acb0b70aa03862d65535d68a18bd62c907d11
Aishik C. <achak0120@gmail.com>
2025-06-04 16:26:00 -0500

    load image files to model

--- src/ai_model/model.py
+++ src/ai_model/model.py
@@ -1,7 +1,18 @@
 import torch
 from torchvision import datasets, transforms
 from torch.utils.data import DataLoader
-import matplotlib as plt
+import matplotlib.pyplot as plt
 import numpy as np
+import cv2
+from torchvision.datasets import ImageFolder
 
-print(matplotlib.__version__)
+#Load image files
+train_path = r'C:\Users\achak\OneDrive\Desktop\rover\AERIS\src\DATA FOLDER\raw\images\cubes_dset\train'
+val_path = r'C:\Users\achak\OneDrive\Desktop\rover\AERIS\src\DATA FOLDER\raw\images\cubes_dset\val'
+traindata = ImageFolder(train_path)
+valdata = ImageFolder(val_path)
+
+
+print("Classes: ", traindata.classes)
+print("Classes: ", valdata.classes)
+
